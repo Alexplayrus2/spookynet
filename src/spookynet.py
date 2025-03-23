d8edf8eaa4b255b2ca364efe6cabd9e28aac85bd
Alexplayrus1 <118437098+Alexplayrus2@users.noreply.github.com>
2025-03-23 17:47:37 +0300

    prevented permission denied

--- src/spookynet.py
+++ src/spookynet.py
@@ -11,7 +11,8 @@ print("Welcome to SpookyNet! Your ID is " + selfid)
 otherid = input("Enter the ID of the person you want to join: ")
 delay = float(input("Enter request delay (recommended: 0 to 1) (float), if you dont see other players and get alot of request errors then raise it, however it will make you laggier, you can set it to 0 if ur using a custom server): "))
 while True:
-    file = open(pathlol + "p2data.ini", "w")
+    try:
+        file = open(pathlol + "p2data.ini", "w")
     try:
         contents = urllib.request.urlopen(serverurl + "get?name=SJM" + otherid).read()
         file.write(contents.decode('utf-8').replace("ENTERSPACE", "\n"))
@@ -20,8 +21,9 @@ while True:
         print("Get request error! " + str(e) + ", see below for extra details")
         logging.exception("GetReqException")
     time.sleep(delay)
-    file = open(pathlol + "p1data.ini", "r")
-    fcontent = file.read()
+    try:
+        file = open(pathlol + "p1data.ini", "r")
+        fcontent = file.read()
     try:
         contents = urllib.request.urlopen(serverurl + "set?name=SJM" + selfid + "&val=" + fcontent.replace("\n", "ENTERSPACE")).read()
         print("Set request success!")
