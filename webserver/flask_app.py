bf02e919804cff6d3aadb73b2b275b307894fec1
Alexplayrus1 <118437098+Alexplayrus2@users.noreply.github.com>
2025-03-23 15:07:40 +0300

    Create flask_app.py

---
+++ webserver/flask_app.py
@@ -0,0 +1,21 @@
+# hosted at https://alexplayrus1.pythonanywhere.com
+
+from flask import Flask
+from flask import request
+import json
+mpdata = "{}"
+app = Flask(__name__)
+
+@app.route("/multiplayer/set")
+def mpset():
+    global mpdata
+    data = json.loads(mpdata)
+    data[request.args.get('name')] = request.args.get('val')
+    mpdata = json.dumps(data)
+    return request.args.get('val')
+
+@app.route("/multiplayer/get")
+def mpget():
+    global mpdata
+    data = json.loads(mpdata)
+    return data[request.args.get('name')]
